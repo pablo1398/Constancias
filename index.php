519788034185bfbc74f008807c1bc862af052a5a
chuyo123 <51136296+chuyo123@users.noreply.github.com>
2021-09-22 18:38:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		margin-top: 30px;
             }
             .btn1{
-                background-color: #2DD76D;
+                background-color: #00A7E1;
                 color: white;
                 border-radius:10px;
             }
