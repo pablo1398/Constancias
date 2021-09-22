3f0c57b498bfe0a9cc6bf9d3a46a969bb4222085
pablo1398 <91106534+pablo1398@users.noreply.github.com>
2021-09-22 01:40:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <html>
-	<body>
-		<h1>Constancias de no queja medica</h1>
-	</body>
+    <head>
+        <style>
+            .header{
+
+            }
+            .recuadro{
+
+            }
+            .titulo{
+
+            }
+            .btn{
+                background-color: #00A7E1;
+                color: white;
+                padding:6px;
+                border-radius:10px;
+            }
+            .btn2{
+                background-color: #E7004C;
+                color: white;
+                padding:6px;
+                border-radius:10px;
+            }
+        </style>
+    </head>
+    <body>
+        <img class="header" src="images/header_salud_00a7e1.jpg">
+        
+        <div class="recuadro">
+            <div class="titulo">
+                Bienvenido
+            </div>
+            <div class="usuario">
+                USUARIO / CORREO
+                <input type="text">
+            </div>
+            <div class="contraseña">
+                CONTRASEÑA
+                <input type="password">
+            </div>
+            <div class="inicio">
+                <button class="btn1">INICIAR SESION</button>
+            </div>
+            <div class="recuperar">
+                <button class="btn2">RECUPERAR CONTRASEÑA</button>
+            </div>
+        </div>
+    </body>
 </html>
